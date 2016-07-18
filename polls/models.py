29f9ac400d1a22828eaff96f077ab783922788b3
Alice Pao <gusjaq@gmail.com>
2016-07-17 17:29:08 -0700

    Add Question model properties for template access and update templates with Skeleton CSS class attributes (complete Django Tutorial Part VII)

--- polls/models.py
+++ polls/models.py
@@ -12,6 +12,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    @property
+    def sorted_choice_set_by_text(self):
+        return self.choice_set.order_by('choice_text')
+
+    @property
+    def sorted_choice_set_by_votes(self):
+        return self.choice_set.order_by('-votes', 'choice_text')
+
     def __str__(self):
         return self.question_text
 
